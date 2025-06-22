701af0728976739b90d0e854de839485fa53ec59
FrancoCaredda <franco.caredda12345@gmail.com>
2025-06-22 14:57:42 +0200

    Add base classes for characters

---
+++ Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -0,0 +1,14 @@
+#include "Characters/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), TEXT("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
